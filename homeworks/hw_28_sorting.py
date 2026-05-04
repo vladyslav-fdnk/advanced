86d154461f65958da5055f5af2492c9d23832d66
Vladyslav <wladyslaw.fedchenko@gmail.com>
2026-05-04 13:30:51 +0200

    homework is done

---
+++ homeworks/hw_28_sorting.py
@@ -0,0 +1,113 @@
+import random
+
+def cocktail_shaker_sort(arr):   #task_1
+    n = len(arr)
+    swapped = True
+    start = 0
+    end = n - 1
+
+    while swapped:
+        swapped = False
+
+        for i in range(start, end):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+
+        if not swapped:
+            break
+
+        swapped = False
+        end -= 1
+
+        for i in range(end - 1, start - 1, -1):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+
+        start += 1
+
+    return arr
+
+def merge_sort(arr):            #task_2
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+
+    left = []
+    right = []
+
+    for i in range(mid):
+        left.append(arr[i])
+
+    for i in range(mid, len(arr)):
+        right.append(arr[i])
+
+    left = merge_sort(left)
+    right = merge_sort(right)
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+
+    return result
+
+
+
+
+def insertion_sort(arr, left, right):           #task_3
+    for i in range(left + 1, right + 1):
+        key = arr[i]
+        j = i - 1
+
+        while j >= left and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+
+def quicksort(arr, low, high, limit=10):
+    if low < high:
+
+        if high - low + 1 <= limit:
+            insertion_sort(arr, low, high)
+            return
+
+        pi = partition(arr, low, high)
+
+        quicksort(arr, low, pi - 1, limit)
+        quicksort(arr, pi + 1, high, limit)
