522d55781052575f552848c58bce992bc9f14bc5
vlad <wladyslaw.fedchenko@gmail.com>
2026-05-09 21:49:02 +0200

    lesson 36 added

--- lessons/async_python.py
+++ lessons/async_python.py
@@ -6,19 +6,22 @@ def slow_sync_input_output():
     time.sleep(1) # mocking slow http call
 
 
-async def slow_async_input_output():
-    await asyncio.sleep(1)
-
-
 def main_sync():
     start=time.perf_counter()
     for _ in range(10):
         slow_sync_input_output()
     print(f'sync time: { time.perf_counter() - start:.2f}s')
 
+
+async def slow_async_input_output():
+    await asyncio.sleep(1)
+
+
 async def main_async():
     start=asyncio.get_event_loop().time()
-    await asyncio.gather(*(slow_async_input_output() for _ in range(10)))
+
+    tasks=[ slow_async_input_output() for _ in range(10) ]
+    await asyncio.gather(*tasks)
     print(f'async time: { time.perf_counter() - start:.2f}s')
 
 if __name__ == '__main__':
